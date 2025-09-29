b77a8c055fc34a0de502a8e57d55858281b66c65
Hilal Ahmad <hilal@archiwiz.com>
2025-09-29 13:46:28 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
         {
             name: "testing app azure",
             script: "node_modules/next/dist/bin/next",
-            agrs: "start",
+            agrs: "start -p 8080",
             env: {
-                PORT: process.env.PORT || 3000,
+                PORT: process.env.PORT || 8080,
             },
             watch: false,
             autorestart: true,
